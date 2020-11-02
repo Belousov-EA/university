ac18e7b0dd21ac6263510af2e509b31432e74049
BLE <Belousov.ea98@gmail.com>
2020-11-02 23:43:13 +0300

    инженерная этика беее

--- Диплом/edges.py
+++ Диплом/edges.py
@@ -12,10 +12,18 @@ def e3(speed, mass):
     
 
 class Bullet:
-    def __init__( mass, energy, speed );
-    self.mass=mass
-    self.energy=energy
-    self.speed=speed
+    def __init__( self, mass, energy, speed );
+        self.mass=mass
+        self.energy=energy
+        self.speed=speed
+    
+    def reverse_constructor( self, mass, energy, speed ):
+        if( mass == NULL ):
+            mass = self.mass
+        if( energy == NULL ):
+            energy = self.energy
+        if( speed == NULL ):
+            speed = self.speed
 
 
 
@@ -30,13 +38,20 @@ def e6(energy, depth):
     
 
 class Wall:
-    def __init__( depth, energy, resistance_force):
+    def __init__( self, depth, energy, resistance_force):
         self.depth = depth
         self.resistance_force=resistance_force
         self.energy=energy
+        
+    def reverse_constructor( self, depth, energy, resistance_force):
+        if( depth == NULL ):
+            depth = self.depth
+        if( energy == NULL ):
+            energy == self.energy
+        if( resistance_force == NULL ):
+            resistance_force = self.resistance_force
     
     
-/* возможно, стоит обыграть через декоратор */
 def to_slow_down(bullet, wall, end_speed):
     wall.energy = bullet.energy - bullet.mass * (end_speed ** 2) / 2
     
@@ -45,3 +60,19 @@ def to_penetrate(bullet, wall, end_speed):
     bullet.energy = wall.energy + bullet.mass * (end_speed ** 2) / 2
 
     
+    
+    
+    
+    
+class Двухкомпонентный_клей:
+    def __init__( компонента_А, компонента_B ):
+        if( компонента_А.химическое_вещество == "Дифенилолпропан" and \
+        компонента_B.химическое_вещество == "Эпихлоргидрин"):
+            self.агрегатное_состояние = "Твердое тело"
+        else:
+            self.агрегатное_состояние = "Жидкость"
+            
+    def обратный_конструктор(компонента_А, компонента_B, агрегатное_состояние ):
+        компонента_А = self.компонента_А
+        компонента_B = self.компонента_B
+        агрегатное_состояние = self.агрегатное_состояние
